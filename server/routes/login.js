330215dc8d14fc2b2ab846530c9da7b3c6961c3c
Luis De La Hoz <luisgdelahozl@gmail.com>
2018-10-11 00:30:32 -0300

    Google Sign In

--- server/routes/login.js
+++ server/routes/login.js
@@ -4,6 +4,9 @@ const jwt = require('jsonwebtoken');
 const Usuario = require('../models/usuario');
 const app = express();
 
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(process.env.CLIENT_ID);
+
 app.post('/login', (req, res) => {
 
     let body = req.body;
@@ -38,6 +41,92 @@ app.post('/login', (req, res) => {
                 ok: true,
                 usuario: usuario,
                 token: token
+            });
+        }
+    });
+});
+
+async function verify(token) {
+
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: process.env.CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+
+    const payload = ticket.getPayload();
+
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    };
+
+}
+
+app.post('/google', async(req, res) => { 
+
+    let token = req.body.idtoken;
+    
+    let googleUser = await verify(token)
+                        .catch((error) => {
+                            return res.status(403).json({
+                                ok: false,
+                                error: error
+                            });
+                        });
+    
+    Usuario.findOne({ email: googleUser.email }, (error, usuario) => {
+        if(error) {
+            return res.status(500).json({
+                ok: false,
+                error: error
+            });
+        } else if(usuario) {
+            if(!usuario.google) {
+                return res.status(500).json({
+                    ok: false,
+                    error: {
+                        message: 'Debe usar su autenticación normal'
+                    }
+                });
+            } else {
+                let token = jwt.sign({
+                    usuario: usuario
+                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+                
+                return res.json({
+                    ok: true,
+                    usuario: usuario,
+                    token: token
+                });
+            }
+        } else {
+            let usuario = new Usuario();
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.google = googleUser.google;
+            usuario.password = ':)';
+
+            usuario.save((error, usuario) => {
+                if(error) {
+                    return res.status(500).json({
+                        ok: false,
+                        error: error
+                    });
+                } else {
+                    let token = jwt.sign({
+                        usuario: usuario
+                    }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+                    
+                    return res.json({
+                        ok: true,
+                        usuario: usuario,
+                        token: token
+                    });
+                }
             });
         }
     });
--- server/server.js
+++ server/server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const bodyParser =  require('body-parser');
+const path = require('path');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 app.use(require('./routes/index'));
 
